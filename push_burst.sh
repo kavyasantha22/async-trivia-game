a161df2ba717e32d27ce24a38e51c63b5dd6ec66
Putu Kavya Santha Guna <124337140+kavyasantha22@users.noreply.github.com>
2025-10-24 21:47:13 +1100

    automatic push

---
+++ push_burst.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Run push.sh 100 times in rapid succession.
+for _ in {1..100}; do
+  ./push.sh
+done
--- push_loop.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Continuously run push.sh with an optional delay (default 60s) between runs.
-interval="${1:-60}"
-
-while true; do
-  ./push.sh
-  sleep "$interval"
-done
