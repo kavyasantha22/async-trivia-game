de3f284fbef3e6adc7a2fd000fe1f9e90a5d5626
Putu Kavya Santha Guna <124337140+kavyasantha22@users.noreply.github.com>
2025-10-28 14:13:38 +1100

    automatic push

--- server.py
+++ server.py
@@ -202,6 +202,7 @@ class Server:
                 elif len(self._sessions) >= self._num_players and question_round_start is None:
                     self._log("Everyone has joined!")
                     ready_msg = self._construct_ready_message()
+                    self._log("Everyone has joined!")
                     question_round_start = cur_time + self._question_interval
                     # print("it goes to the right branch")
                     await self._broadcast(ready_msg)
